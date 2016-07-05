011e04535236d0040a84a794af3d71cb9b117e03
Olivia Taylor <obt1@pitt.edu>
2016-07-05 08:28:13 -0400

    added variable and saved move in short attention

--- RockPaperScissors/ShortAttentionSpanAI.cs
+++ RockPaperScissors/ShortAttentionSpanAI.cs
@@ -8,15 +8,25 @@ namespace RockPaperScissors
 {
     class ShortAttentionSpanAI : IPlayer
     {
+        //public int save players move
+        //Properties
+        public int PlayersMove;
+
+
+        //Methods
         public int NextMove()
         {
+         
+            if()
+                
             throw new NotImplementedException();
         }
 
         public void SaveResult(int myMove, int otherMove)
         {
-           
-            throw new NotImplementedException();
+            //save other move into my variable
+            PlayersMove = otherMove;
+
         }
     }
 }
